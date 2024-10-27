23c5653a9c265669ecf9f16ffee9ec29989cb9e9
Marija Bakic <marija99bakic@gmail.com>
2024-10-27 15:31:37 +0100

    Dodata is_admin kolona da bi postojale razlicite uloge

--- kozmeticki_salon/database/seeders/UserSeeder.php
+++ kozmeticki_salon/database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 use App\Models\User;
 
@@ -14,6 +15,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'name'=>"Marija",
+            'email'=>"marija@gmail.com",
+            'password' =>  "marija123",
+            'is_admin' => true,
+            'remember_token' => Str::random(10),
+        ]);
+       
         User::factory()->times(6)->create();
     }
 }
